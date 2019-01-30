3034f8e6aff0daec0b08782b996dd08691c17e22
vineethk123 <vineeth.kolluru@zemosolabs.com>
2019-01-30 23:38:50 +0530

    make nav-bar load from a separate html file, add a form to contact me page.

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
+/** Check if the details entered are valid and show a status pop-up */
 validateForm = () => {
   let nameInputElement = document.getElementById("sender-name");
   let emailInputElement = document.getElementById("sender-email");
   let formElement = document.getElementById("contact-form");
   if (nameInputElement.checkValidity() && emailInputElement.checkValidity()) {
-    alert("Deatils Successfully Submitted");
+    alert("Details Successfully Submitted");
     formElement.reset();
     return true;
   } else {
@@ -11,3 +12,17 @@ validateForm = () => {
     return false;
   }
 };
+
+readImageInfo = () => {
+  $.get("resources/image_info.json", function(jsonobj) {});
+  /*let imagesInformation = JSON.parse(jsonImagesInformation);
+  for (const imageInformation of imagesInformation) {
+    document.getElementById(imageInformation["id"]).getAttribute("src") = imageInformation['imageURL'];
+  }*/
+};
+
+readImageInfo();
+// Load nav-bar from a html file.
+$(function() {
+  $(".nav-container").load("nav-bar.html");
+});
