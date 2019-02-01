19868c632be2c7d468782bdf2795f3a72e878adc
vineethk123 <vineeth.kolluru@zemosolabs.com>
2019-02-01 12:43:07 +0530

    remove hovering icons in gallery page.

--- js/gallery-admin.js
+++ js/gallery-admin.js
@@ -1,48 +1,49 @@
-let stringImagesInformation = `{
-    "image-1": {
-      "imageURL": "https://images.unsplash.com/photo-1476610182048-b716b8518aae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=827&q=80",
-      "name": "Grass Land",
-      "description": "Photo by Robert Lukeman",
-      "uploadDate": "2016-10-16"
-    },
-    "image-2": {
-      "imageURL": "https://images.unsplash.com/photo-1529528070131-eda9f3e90919?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=375&q=80",
-      "name": "Sea Shore",
-      "description": "Photo by Dan Grinwis",
-      "uploadDate": "2018-06-20"
-    },
-    "image-3": {
-      "imageURL": "https://images.unsplash.com/photo-1504714146340-959ca07e1f38?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=625&q=80",
-      "name": "Floating Houses",
-      "description": "Photo by Ali Al Mufti",
-      "uploadDate": "2017-09-06"
-    },
-    "image-4": {
-      "imageURL": "https://images.unsplash.com/photo-1491466424936-e304919aada7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=749&q=80",
-      "name": "Aurora",
-      "description": "Photo by Jonatan Pie",
-      "uploadDate": "2017-04-06"
-    },
-    "image-5": {
-      "imageURL": "https://images.unsplash.com/photo-1502318217862-aa4e294ba657?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=358&q=80",
-      "name": "The Great Rift",
-      "description": "Photo by Nate Rayfield",
-      "uploadDate": "2017-09-09"
-    },
-    "image-6": {
-      "imageURL": "https://images.unsplash.com/photo-1452990457935-b5638f0c013b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
-      "name": "Sunlight Through Trees",
-      "description": "Photo by Darren Bockman",
-      "uploadDate": "2016-01-07"
-    }
-  }`;
+const stringImagesInformation = `{
+  "image-1": {
+    "imageURL": "https://images.unsplash.com/photo-1476610182048-b716b8518aae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=827&q=80",
+    "name": "Grass Land",
+    "description": "Photo by Robert Lukeman",
+    "uploadDate": "2016-10-16"
+  },
+  "image-2": {
+    "imageURL": "https://images.unsplash.com/photo-1529528070131-eda9f3e90919?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=375&q=80",
+    "name": "Sea Shore",
+    "description": "Photo by Dan Grinwis",
+    "uploadDate": "2018-06-20"
+  },
+  "image-3": {
+    "imageURL": "https://images.unsplash.com/photo-1504714146340-959ca07e1f38?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=625&q=80",
+    "name": "Floating Houses",
+    "description": "Photo by Ali Al Mufti",
+    "uploadDate": "2017-09-06"
+  },
+  "image-4": {
+    "imageURL": "https://images.unsplash.com/photo-1491466424936-e304919aada7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=749&q=80",
+    "name": "Aurora",
+    "description": "Photo by Jonatan Pie",
+    "uploadDate": "2017-04-06"
+  },
+  "image-5": {
+    "imageURL": "https://images.unsplash.com/photo-1502318217862-aa4e294ba657?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=358&q=80",
+    "name": "The Great Rift",
+    "description": "Photo by Nate Rayfield",
+    "uploadDate": "2017-09-09"
+  },
+  "image-6": {
+    "imageURL": "https://images.unsplash.com/photo-1452990457935-b5638f0c013b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+    "name": "Sunlight Through Trees",
+    "description": "Photo by Darren Bockman",
+    "uploadDate": "2016-01-07"
+  }
+}`;
 
-const imagesContainer = document.getElementById("all-image-container");
-let modal = document.getElementById("modal");
 // Store images information in local storage and retreive it as a JSON object.
 localStorage.setItem("images-information", stringImagesInformation);
 let retrievedImagesInformation = localStorage.getItem("images-information");
 let jsonImagesInformation = JSON.parse(retrievedImagesInformation);
+
+const imagesContainer = document.getElementById("all-image-container");
+let modal = document.getElementById("modal");
 
 /**
  * Take a JSON object containing multiple images information and render them inside div with
---
+++ js/images-information.js
@@ -0,0 +1,40 @@
+const stringImagesInformation = `{
+    "image-1": {
+      "imageURL": "https://images.unsplash.com/photo-1476610182048-b716b8518aae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=827&q=80",
+      "name": "Grass Land",
+      "description": "Photo by Robert Lukeman",
+      "uploadDate": "2016-10-16"
+    },
+    "image-2": {
+      "imageURL": "https://images.unsplash.com/photo-1529528070131-eda9f3e90919?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=375&q=80",
+      "name": "Sea Shore",
+      "description": "Photo by Dan Grinwis",
+      "uploadDate": "2018-06-20"
+    },
+    "image-3": {
+      "imageURL": "https://images.unsplash.com/photo-1504714146340-959ca07e1f38?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=625&q=80",
+      "name": "Floating Houses",
+      "description": "Photo by Ali Al Mufti",
+      "uploadDate": "2017-09-06"
+    },
+    "image-4": {
+      "imageURL": "https://images.unsplash.com/photo-1491466424936-e304919aada7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=749&q=80",
+      "name": "Aurora",
+      "description": "Photo by Jonatan Pie",
+      "uploadDate": "2017-04-06"
+    },
+    "image-5": {
+      "imageURL": "https://images.unsplash.com/photo-1502318217862-aa4e294ba657?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=358&q=80",
+      "name": "The Great Rift",
+      "description": "Photo by Nate Rayfield",
+      "uploadDate": "2017-09-09"
+    },
+    "image-6": {
+      "imageURL": "https://images.unsplash.com/photo-1452990457935-b5638f0c013b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+      "name": "Sunlight Through Trees",
+      "description": "Photo by Darren Bockman",
+      "uploadDate": "2016-01-07"
+    }
+  }`;
+
+export { stringImagesInformation };
